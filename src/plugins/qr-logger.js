626b0b7e46282a517221738120b845f438593410
Thiago Jurge <thiagojurge@hotmail.com>
2025-05-25 22:56:35 -0300

    Add express and qrcode dependencies; implement QR code server

--- src/plugins/qr-logger.js
+++ src/plugins/qr-logger.js
@@ -1,8 +1,28 @@
-import qrcode from 'qrcode-terminal';
+import express from 'express';
+import qrcode from 'qrcode';
 
 export default async function qrLogger(client) {
-  client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
+  const app = express();
+  let qrImageData = '';
+
+  client.on('qr', async (qr) => {
+    qrImageData = await qrcode.toDataURL(qr);
+    console.log('🖼️ QR code disponível em http://localhost:3000');
+  });
+
+  app.get('/', (req, res) => {
+    if (!qrImageData) {
+      return res.send('QR code não gerado ainda. Aguarde...');
+    }
+
+    res.send(`
+      <h2>Escaneie o QR code com o WhatsApp:</h2>
+      <img src="${qrImageData}" />
+    `);
+  });
+
+  app.listen(3000, () => {
+    console.log('🌐 Servidor de QR rodando em http://localhost:3000');
   });
 
   client.on('ready', () => {
